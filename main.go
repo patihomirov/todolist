f54732ab52ca59ab7248d7f83e7f00ed57198fc0
patihomirov <patihomirov@gmail.com>
2022-05-15 01:50:03 +0300

    Реализован функционал по html-запросу: создание задач, изменение статуса задач, очистка завершенных задач

--- main.go
+++ main.go
@@ -7,22 +7,24 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
-	"strconv"
+	"time"
 )
 
 var database, _ = sql.Open("sqlite3", "./todobase.db")
 
 func main() {
 	//Инициализация БД
-
 	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, task TEXT, taskstatus TEXT, creationdate TEXT, closeddate TEXT)")
 	statement.Exec()
 
 	//Обработка внешних запросов
 	router := mux.NewRouter()
 	router.HandleFunc("/", sHead)
-	router.HandleFunc("/create", sCreate)
+	router.HandleFunc("/create/{taskname}", sCreate)
+	router.HandleFunc("/changestatus/{taskid}/{taskstatus}", sStatusChange)
 	router.HandleFunc("/taskPrint", sPrint)
+	router.HandleFunc("/clearall", sClearAll)
+	router.HandleFunc("/cleardone", sClearDone)
 	//	router.HandleFunc("/clear",sClear)
 	//	router.HandleFunc("/list",sList)
 	//	router.HandleFunc("/feedback/{feedback}",sFeedback)
@@ -39,16 +41,14 @@ func sHead(w http.ResponseWriter, r *http.Request) {
 
 //Создание задач
 func sCreate(w http.ResponseWriter, r *http.Request) {
-	statement, _ := database.Prepare(`INSERT INTO tasks (task, taskstatus, creationdate ) VALUES (?, "opened", datetime())`)
-	//Тестируем - создаем 10 задач
-	for i := 1; i <= 10; i++ {
-		statement.Exec("task №" + strconv.Itoa(i))
-	}
-	w.Write([]byte("Tasks Created"))
+	vars := mux.Vars(r)
+	taskname := vars["taskname"]
+	statement, _ := database.Prepare(`INSERT INTO tasks (task, taskstatus, creationdate, closeddate ) VALUES (?, "opened", ?, "")`)
+	statement.Exec(taskname, time.Now().String())
+	w.Write([]byte(`Tasks "` + taskname + `" Created`))
 }
 
 func sPrint(w http.ResponseWriter, r *http.Request) {
-
 	w.Write([]byte(taskPrint()))
 }
 
@@ -61,11 +61,40 @@ func taskPrint() string {
 	var taskstatus string
 	var creationdate string
 	var closeddate string
+	printbuffer = fmt.Sprintf("ID \t Задача: \t Статуc: \t Дата открытия: \t Дата закрытия: \n")
 	for rows.Next() {
 		rows.Scan(&id, &task, &taskstatus, &creationdate, &closeddate)
-		//printbuffer = append(printbuffer, fmt.Sprintf("%d: %s %s %s %s\n", id, task, taskstatus, creationdate, closeddate))
-		printbuffer = printbuffer + fmt.Sprintf("%d: %s %s %s %s\n", id, task, taskstatus, creationdate, closeddate)
+		printbuffer = printbuffer + fmt.Sprintf("%d:\t %s \t %s \t %s \t %s \n", id, task, taskstatus, creationdate, closeddate)
 	}
 	return printbuffer
-	//w.Write([]byte("Tasks Created"))
+}
+
+//Изменение статуса задачи
+func sStatusChange(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	taskid := vars["taskid"]
+	taskstatus := vars["taskstatus"]
+	var taskclosedate string
+	if taskstatus == "done" {
+		taskclosedate = time.Now().String()
+	} else {
+		taskclosedate = ""
+	}
+	statement, _ := database.Prepare(`UPDATE tasks SET taskstatus = ?, closeddate = ? WHERE id = ?`)
+	statement.Exec(taskstatus, taskclosedate, taskid)
+	w.Write([]byte(`Tasks "` + taskid + `" change status to ` + taskstatus))
+}
+
+//Удаление из БД завершенных задач
+func sClearDone(w http.ResponseWriter, r *http.Request) {
+	statement, _ := database.Prepare(`DELETE FROM tasks WHERE taskstatus = ?`)
+	statement.Exec("done")
+	w.Write([]byte("Clear doned tasks done!"))
+}
+
+//Удаление из БД всех записей
+func sClearAll(w http.ResponseWriter, r *http.Request) {
+	statement, _ := database.Prepare(`DELETE FROM tasks`)
+	statement.Exec()
+	w.Write([]byte("Clear all tasks done!"))
 }
